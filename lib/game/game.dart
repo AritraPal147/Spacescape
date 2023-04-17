dc3d6ccce4780471c4e4b56f9ae22fbfe9502d59
Aritra Pal <94114400+AritraPal147@users.noreply.github.com>
2023-04-18 02:31:04 +0530

    completed app

--- lib/game/game.dart
+++ lib/game/game.dart
@@ -1,187 +1,335 @@
-import 'package:flame/components.dart';
-import 'package:flame/events.dart';
-import 'package:flame/extensions.dart';
 import 'package:flame/game.dart';
+import 'package:flame/input.dart';
+import 'package:flame/parallax.dart';
 import 'package:flame/sprite.dart';
+import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
-import 'package:spacescape/game/bullet.dart';
-import 'package:spacescape/game/enemy.dart';
-import 'package:spacescape/game/enemy_manager.dart';
-import 'package:spacescape/game/player.dart';
+import 'package:provider/provider.dart';
 
-// This class is responsible for initializing and running the game-loop
+import '../widgets/overlays/pause_menu.dart';
+import '../widgets/overlays/pause_button.dart';
+import '../widgets/overlays/game_over_menu.dart';
+
+import '../models/player_data.dart';
+import '../models/spaceship_details.dart';
+
+import 'enemy.dart';
+import 'health_bar.dart';
+import 'player.dart';
+import 'bullet.dart';
+import 'command.dart';
+import 'power_ups.dart';
+import 'enemy_manager.dart';
+import 'power_up_manager.dart';
+import 'audio_player_component.dart';
+
+// This class is responsible for initializing and running the game-loop.
 class SpacescapeGame extends FlameGame
     with
-        HasGameRef,
-        PanDetector,
-        TapDetector {
-  // Stores a reference to player component
-  late Player player;
+        HasDraggables,
+        HasTappables,
+        HasCollisionDetection,
+        HasKeyboardHandlerComponents {
+  // Stores a reference to player component.
+  late Player _player;
 
-  // Stores a reference to the main sprite sheet.
-  late SpriteSheet _spriteSheet;
+  // Stores a reference to the main spritesheet.
+  late SpriteSheet spriteSheet;
 
   // Stores a reference to an enemy manager component.
   late EnemyManager _enemyManager;
 
-  // These represent the start and updated pointer position.
-  // Null values represent that the user is not touching the screen at the moment.
-  Offset? _pointerStartPosition;
-  Offset? _pointerCurrentPosition;
+  // Stores a reference to an power-up manager component.
+  late PowerUpManager _powerUpManager;
 
-  // Controls how big the joystick zone is.
-  final double _joystickRadius = 60;
+  // Displays player score on top left.
+  late TextComponent _playerScore;
 
-  // Controls how big the joystick thumb is.
-  final double _joystickThumbRadius = 20;
+  // Displays player helth on top right.
+  late TextComponent _playerHealth;
 
-  // Controls how big the dead zone is.
-  final double _deadZoneRadius = 10;
+  late AudioPlayerComponent _audioPlayerComponent;
+
+  // List of commands to be processed in current update.
+  final _commandList = List<Command>.empty(growable: true);
+
+  // List of commands to be processed in next update.
+  final _addLaterCommandList = List<Command>.empty(growable: true);
+
+  // Indicates wheater the game world has been already initilized.
+  bool _isAlreadyLoaded = false;
 
   // This method gets called by Flame before the game-loop begins.
   // Assets loading and adding component should be done here.
   @override
   Future<void> onLoad() async {
-    // Loads the sprite sheet
-    await images.load('simpleSpace_tilesheet@2.png');
+    // Makes the game use a fixed resolution irrespective of the windows size.
+    camera.viewport = FixedResolutionViewport(Vector2(540, 960));
 
-    // Divides the sprite sheet into rows and columns for
-    // easier access
-    _spriteSheet = SpriteSheet.fromColumnsAndRows(
-      image: images.fromCache('simpleSpace_tilesheet@2.png'),
-      columns: 8,
-      rows: 6,
-    );
+    // Initilize the game world only one time.
+    if (!_isAlreadyLoaded) {
+      // Loads and caches all the images for later use.
+      await images.loadAll([
+        'simpleSpace_tilesheet@2.png',
+        'freeze.png',
+        'icon_plusSmall.png',
+        'multi_fire.png',
+        'nuke.png',
+      ]);
 
-    // Chooses a sprite and places it on the screen
-    player = Player(
-      sprite: _spriteSheet.getSpriteById(17),
-      size: Vector2(64, 64),
-      position: gameRef.size / 2,
-    );
+      _audioPlayerComponent = AudioPlayerComponent();
+      add(_audioPlayerComponent);
 
-    // Makes sure that the sprite is centered
-    // default anchor is Top Left
-    player.anchor = Anchor.center;
-    add(player);
-    
-    _enemyManager = EnemyManager(spriteSheet: _spriteSheet);
-    add(_enemyManager);
+      final stars = await ParallaxComponent.load(
+        [ParallaxImageData('stars1.png'), ParallaxImageData('stars2.png')],
+        repeat: ImageRepeat.repeat,
+        baseVelocity: Vector2(0, -50),
+        velocityMultiplierDelta: Vector2(0, 1.5),
+      );
+      add(stars);
+
+      spriteSheet = SpriteSheet.fromColumnsAndRows(
+        image: images.fromCache('simpleSpace_tilesheet@2.png'),
+        columns: 8,
+        rows: 6,
+      );
+
+      // Create a basic joystick component on left.
+      final joystick = JoystickComponent(
+        anchor: Anchor.bottomLeft,
+        position: Vector2(30, size.y - 30),
+        // size: 100,
+        background: CircleComponent(
+          radius: 60,
+          paint: Paint()..color = Colors.white.withOpacity(0.5),
+        ),
+        knob: CircleComponent(radius: 30),
+      );
+      add(joystick);
+
+      /// As build context is not valid in onLoad() method, we
+      /// cannot get current [PlayerData] here. So initilize player
+      /// with the default SpaceshipType.Canary.
+      const spaceshipType = SpaceshipType.canary;
+      final spaceship = Spaceship.getSpaceshipByType(spaceshipType);
+
+      _player = Player(
+        joystick: joystick,
+        spaceshipType: spaceshipType,
+        sprite: spriteSheet.getSpriteById(spaceship.spriteId),
+        size: Vector2(64, 64),
+        position: size / 2,
+      );
+
+      // Makes sure that the sprite is centered.
+      _player.anchor = Anchor.center;
+      add(_player);
+
+      _enemyManager = EnemyManager(spriteSheet: spriteSheet);
+      add(_enemyManager);
+
+      _powerUpManager = PowerUpManager();
+      add(_powerUpManager);
+
+      // Create a fire button component on right
+      final button = ButtonComponent(
+        button: CircleComponent(
+          radius: 60,
+          paint: Paint()..color = Colors.white.withOpacity(0.5),
+        ),
+        anchor: Anchor.bottomRight,
+        position: Vector2(size.x - 30, size.y - 30),
+        onPressed: _player.joystickAction,
+      );
+      add(button);
+
+      // Create text component for player score.
+      _playerScore = TextComponent(
+        text: 'Score: 0',
+        position: Vector2(10, 10),
+        textRenderer: TextPaint(
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 12,
+            fontFamily: 'BungeeInline',
+          ),
+        ),
+      );
+
+      // Setting positionType to viewport makes sure that this component
+      // does not get affected by camera's transformations.
+      _playerScore.positionType = PositionType.viewport;
+
+      add(_playerScore);
+
+      // Create text component for player health.
+      _playerHealth = TextComponent(
+        text: 'Health: 100%',
+        position: Vector2(size.x - 10, 10),
+        textRenderer: TextPaint(
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 12,
+            fontFamily: 'BungeeInline',
+          ),
+        ),
+      );
+
+      // Anchor to top right as we want the top right
+      // corner of this component to be at a specific position.
+      _playerHealth.anchor = Anchor.topRight;
+
+      // Setting positionType to viewport makes sure that this component
+      // does not get affected by camera's transformations.
+      _playerHealth.positionType = PositionType.viewport;
+
+      add(_playerHealth);
+
+      // Add the blue bar indicating health.
+      add(
+        HealthBar(
+          player: _player,
+          position: _playerHealth.positionOfAnchor(Anchor.topLeft),
+          priority: -1,
+        ),
+      );
+
+      // Set this to true so that we do not initilize
+      // everything again in the same session.
+      _isAlreadyLoaded = true;
+    }
   }
 
-  // Adds joystick
-  // Render method comes from Flame's Game class and gets called for every iteration
-  // of game loop. It exposes the underlying Canvas on which components are being drawn.
+  // This method gets called when game instance gets attached
+  // to Flutter's widget tree.
   @override
-  void render(Canvas canvas) {
-    super.render(canvas);
-
-    // Checks if user is touching the screen or not
-    if (_pointerStartPosition != null) {
-      // If user touches the screen, draw a circle on it
-      canvas.drawCircle(
-          _pointerStartPosition!,
-          _joystickRadius,
-          // withAlpha() is used to add a bit of transparency to the circle
-          Paint()..color = Colors.grey.withAlpha(100),
-      );
+  void onAttach() {
+    if (buildContext != null) {
+      // Get the PlayerData from current build context without registering a listener.
+      final playerData = Provider.of<PlayerData>(buildContext!, listen: false);
+      // Update the current spaceship type of player.
+      _player.setSpaceshipType(playerData.spaceshipType);
     }
-
-    if (_pointerCurrentPosition != null) {
-      var delta = _pointerCurrentPosition! - _pointerStartPosition!;
-
-      // Checks if magnitude of delta is greater than 60, i.e. the smaller circle
-      // does not go outside the bigger circle which has radius of 60
-      if (delta.distance > _joystickRadius) {
-        delta = _pointerStartPosition! +
-            (Vector2(delta.dx, delta.dy).normalized() * _joystickRadius).toOffset();
-      } else {
-        delta = _pointerCurrentPosition!;
-      }
-
-      canvas.drawCircle(
-        delta,
-        _joystickThumbRadius,
-        Paint()..color = Colors.white.withAlpha(100),
-      );
-    }
+    _audioPlayerComponent.playBgm('9. Space Invaders.wav');
+    super.onAttach();
   }
+
+  @override
+  void onDetach() {
+    _audioPlayerComponent.stopBgm();
+    super.onDetach();
+  }
+
+  // ===================================
+  // IMPORTANT NOTE
+  // Those overrides are obsolete since Flame v1.2.0 version
+  // This code remains as is as a reference for the YouTube tutorial.
+  // ===================================
+  // @override
+  // void prepare(Component c) {
+  //   super.prepare(c);
+
+  //   // If the component being prepared is of type KnowsGameSize,
+  //   // call onResize() on it so that it stores the current game screen size.
+  //   if (c is KnowsGameSize) {
+  //     c.onResize(size);
+  //   }
+  // }
+
+  // @override
+  // void onResize(Vector2 canvasSize) {
+  //   super.onResize(canvasSize);
+
+  //   // Loop over all the components of type KnowsGameSize and resize then as well.
+  //   children.whereType<KnowsGameSize>().forEach((component) {
+  //     component.onResize(size);
+  //   });
+  // }
+  // ===================================
 
   @override
   void update(double dt) {
     super.update(dt);
 
-    // Removes both enemy and bullet when they collide.
-    for (final enemy in children.whereType<Enemy>()) {
-      for (final bullet in children.whereType<Bullet>()) {
-        // containsPoint() returns true if given point lies inside rectangle of current sprite
-        if (enemy.containsPoint(bullet.absoluteCenter)) {
-          enemy.removeFromParent();
-          bullet.removeFromParent();
-          break;
+    // Run each command from _commandList on each
+    // component from components list. The run()
+    // method of Command is no-op if the command is
+    // not valid for given component.
+    for (var command in _commandList) {
+      for (var component in children) {
+        command.run(component);
+      }
+    }
+
+    // Remove all the commands that are processed and
+    // add all new commands to be processed in next update.
+    _commandList.clear();
+    _commandList.addAll(_addLaterCommandList);
+    _addLaterCommandList.clear();
+
+    if (_player.isMounted) {
+      // Update score and health components with latest values.
+      _playerScore.text = 'Score: ${_player.score}';
+      _playerHealth.text = 'Health: ${_player.health}%';
+
+      /// Display [GameOverMenu] when [Player.health] becomes
+      /// zero and camera stops shaking.
+      if (_player.health <= 0 && (!camera.shaking)) {
+        pauseEngine();
+        overlays.remove(PauseButton.id);
+        overlays.add(GameOverMenu.id);
+      }
+    }
+  }
+
+  // This method handles state of app and pauses
+  // the game when necessary.
+  @override
+  void lifecycleStateChange(AppLifecycleState state) {
+    switch (state) {
+      case AppLifecycleState.resumed:
+        break;
+      case AppLifecycleState.inactive:
+      case AppLifecycleState.paused:
+      case AppLifecycleState.detached:
+        if (_player.health > 0) {
+          pauseEngine();
+          overlays.remove(PauseButton.id);
+          overlays.add(PauseMenu.id);
         }
-      }
-
-      if (player.containsPoint(enemy.absoluteCenter)) {
-        print("Enemy hits player");
-      }
+        break;
     }
+
+    super.lifecycleStateChange(state);
   }
 
-  // Finds out the position at which the screen was touched at the
-  // start and stores it in _pointerStartPosition
-  @override
-  void handlePanStart(DragStartDetails details) {
-    // Initially, both small and big circles will be placed at the same location.
-    _pointerStartPosition = details.globalPosition;
-    _pointerCurrentPosition = details.globalPosition;
+  // Adds given command to command list.
+  void addCommand(Command command) {
+    _addLaterCommandList.add(command);
   }
 
-  @override
-  void handlePanUpdate(DragUpdateDetails details) {
-    // Stores current position of pointer
-    _pointerCurrentPosition = details.globalPosition;
+  // Resets the game to inital state. Should be called
+  // while restarting and exiting the game.
+  void reset() {
+    // First reset player, enemy manager and power-up manager .
+    _player.reset();
+    _enemyManager.reset();
+    _powerUpManager.reset();
 
-    // delta is a vector from pointerStartPosition to pointerCurrentPosition
-    var delta = _pointerCurrentPosition! - _pointerStartPosition!;
+    // Now remove all the enemies, bullets and power ups
+    // from the game world. Note that, we are not calling
+    // Enemy.destroy() because it will unnecessarily
+    // run explosion effect and increase players score.
+    children.whereType<Enemy>().forEach((enemy) {
+      enemy.removeFromParent();
+    });
 
-    // Creates a 'dead zone' for the joystick, which allows the user to stop
-    // spaceship movement by keeping the joystick close to center
-    if (delta.distance > _deadZoneRadius) {
-      player.setMoveDirection(Vector2(delta.dx, delta.dy));
-    } else {
-      player.setMoveDirection(Vector2.zero());
-    }
-  }
+    children.whereType<Bullet>().forEach((bullet) {
+      bullet.removeFromParent();
+    });
 
-  // User is not touching the screen, so set _pointerStartPosition as null
-  @override
-  void handlePanEnd(DragEndDetails details) {
-    _pointerStartPosition = null;
-    _pointerCurrentPosition = null;
-    player.setMoveDirection(Vector2.zero());
-  }
-
-  // User is not touching the screen, so set _pointerStartPosition as null
-  // and sprite's move direction is set to zero, i.e., sprite stops moving
-  @override
-  void onPanCancel() {
-    _pointerStartPosition = null;
-    _pointerCurrentPosition = null;
-    player.setMoveDirection(Vector2.zero());
-  }
-
-  @override
-  void handleTapDown(TapDownDetails details) {
-    super.handleTapDown(details);
-
-    Bullet bullet = Bullet(
-      sprite: _spriteSheet.getSpriteById(28),
-      size: Vector2(64, 64),
-      position: player.position.clone(),
-    );
-
-    bullet.anchor = Anchor.center;
-    add(bullet);
+    children.whereType<PowerUp>().forEach((powerUp) {
+      powerUp.removeFromParent();
+    });
   }
 }
